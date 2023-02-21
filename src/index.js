41f2ab3a510cf02a46718b5ed360769a7dcece92
wx-yz <57770159+wx-yz@users.noreply.github.com>
2023-02-21 17:50:53 +0530

    Removing strict mode in dev - fixes the multiple reloading of components on load time

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
     <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="true"></script>
     <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossOrigin="true"></script>
     <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="true"></script>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
